16d4adbf3b1d3a6c943916b8740c5eb4811d5b50
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-02-19 12:52:26 +0100

    Create 03_multi_line_graph.py
    Purpose: Drawing a multiline graph with matplotlib.pyplot

---
+++ 03_multi_line_graph.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Python 3.9.5
+
+# 03_multi_line_graph.py
+
+# Purpose: Drawing a multiline graph
+
+# Dependencies
+import math
+import matplotlib.pyplot as plt
+
+# Producing data with the functions sin(x), cos(x):
+x = np.arange(0, 15, 0.1)
+sin_x = [math.sin(i) for i in x]
+cos_x = [math.cos(i) for i in x]
+
+# Setting parameters for mutiline graph visualization:
+labels = ['sin(x)', 'cos(x)']
+plt.plot(x, sin_x)
+plt.plot(x, cos_x)
+plt.legend(labels, loc='lower right')
+plt.grid()
+plt.show()
