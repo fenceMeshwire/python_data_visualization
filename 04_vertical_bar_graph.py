11b4ca6628d28e718d7613d542fd2aa16959630f
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-02-22 00:04:44 +0100

    Create 04_vertical_bar_graph.py

---
+++ 04_vertical_bar_graph.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Python 3.9.5
+
+# 04_vertical_bar_graph.py
+
+# Purpose: Visualization of data with a vertical bar graph
+
+# Dependencies
+import matplotlib.pyplot as plt
+import random
+
+# Data
+x = [1, 2, 3, 4, 5]
+y = [random.randint(10, 50) for _ in range(5)]
+
+# Visualization
+label = ['Random data']
+plt.bar(x, y)
+plt.xticks(rotation=90)
+plt.legend(label, loc='upper right')
+plt.show()
