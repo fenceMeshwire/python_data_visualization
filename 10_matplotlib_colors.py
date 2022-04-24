45e3d78d281e1818c8949ef007b1c379822d75a1
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-04-24 15:14:41 +0200

    Create 10_matplotlib_colors.py

---
+++ 10_matplotlib_colors.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Python 3.9.5
+
+# 10_matplotlib_colors.py
+
+# Dependency
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Style
+plt.style.use('seaborn-whitegrid')
+
+# Define figure and axes
+figure = plt.figure()
+axes = plt.axes()
+
+
+x = np.linspace(0, 10, 1000)
+
+plt.plot(x, np.sin(x - 0), color='#00AA22') # Hex code (RRGGBB from 00 to FF)
+plt.plot(x, np.sin(x - 1), color='#001122') # Hex code (RRGGBB from 00 to FF)
+plt.plot(x, np.sin(x - 2), color='#AA1122') # Hex code (RRGGBB from 00 to FF)
+
+plt.show()
